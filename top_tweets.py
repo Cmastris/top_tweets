169a69d039dd539232a97a71fcbe3423096b82b0
Chris Mastris <cmastris@hotmail.com>
2021-06-05 16:28:57 +0100

    Add program design notes

--- top_tweets.py
+++ top_tweets.py
@@ -1,3 +1,17 @@
 import tweepy
 
 import config
+
+# Tweepy authentication func
+
+
+# Account class representing a single Twitter user account
+# Attributes: account name, account handle, Tweets (list of Tweet)
+# Methods:
+# - fetch account's public Tweets (using Tweepy)
+# - sort public Tweets (Likes or Retweets)
+# - return top Tweets (top number or percentage based on sort criteria)
+
+
+# Tweet class representing a single Tweet
+# Attributes: Account, text, hashtags, publish date, Likes, Retweets, number of replies

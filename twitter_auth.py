61b3bcaf24f2293374885399be0493e9ed018239
Chris Mastris <cmastris@hotmail.com>
2021-06-06 14:59:08 +0100

    Implement tweepy_auth()

--- twitter_auth.py
+++ twitter_auth.py
@@ -1,9 +1,13 @@
 import tweepy
 
+import config
+
 
 def tweepy_auth():
     """Return an authenticated Tweepy API object."""
-    # TODO
-    pass
+    auth = tweepy.OAuthHandler(config.CONSUMER_KEY, config.CONSUMER_SECRET)
+    auth.set_access_token(config.ACCESS_TOKEN, config.ACCESS_TOKEN_SECRET)
+    return tweepy.API(auth)
+
 
 api = tweepy_auth()

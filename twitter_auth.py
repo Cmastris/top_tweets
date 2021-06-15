8f899fd93e7ad172c3a14958be02df4c704bfee7
Chris Mastris <cmastris@hotmail.com>
2021-06-15 18:28:32 +0100

    Configure retries for Tweepy API

--- twitter_auth.py
+++ twitter_auth.py
@@ -7,7 +7,7 @@ def tweepy_auth():
     """Return an authenticated Tweepy API object."""
     auth = tweepy.OAuthHandler(config.CONSUMER_KEY, config.CONSUMER_SECRET)
     auth.set_access_token(config.ACCESS_TOKEN, config.ACCESS_TOKEN_SECRET)
-    return tweepy.API(auth)
+    return tweepy.API(auth, retry_count=10, retry_delay=30)
 
 
-api = tweepy_auth()
+API = tweepy_auth()

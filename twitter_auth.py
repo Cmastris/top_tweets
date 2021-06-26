4368995b4895e63a3db9b9962fad825e437fc809
Chris Mastris <cmastris@hotmail.com>
2021-06-26 12:42:24 +0100

    Disable API retries during dev

--- twitter_auth.py
+++ twitter_auth.py
@@ -7,7 +7,8 @@ def tweepy_auth():
     """Return an authenticated Tweepy API object."""
     auth = tweepy.OAuthHandler(config.CONSUMER_KEY, config.CONSUMER_SECRET)
     auth.set_access_token(config.ACCESS_TOKEN, config.ACCESS_TOKEN_SECRET)
-    return tweepy.API(auth, retry_count=10, retry_delay=30)
+    # TODO: set retry values (10, 30) for production use
+    return tweepy.API(auth, retry_count=0, retry_delay=0)
 
 
 API = tweepy_auth()

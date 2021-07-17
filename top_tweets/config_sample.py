651b47c7f7f2e1b89e9409a21076554408ea7e15
Chris Mastris <cmastris@hotmail.com>
2021-07-17 10:57:01 +0100

    Add `SOURCE_USERNAMES` to config.py

--- top_tweets/config_sample.py
+++ top_tweets/config_sample.py
@@ -3,3 +3,10 @@ CONSUMER_KEY = ""
 CONSUMER_SECRET = ""
 ACCESS_TOKEN = ""
 ACCESS_TOKEN_SECRET = ""
+
+# Twitter usernames representing accounts whose Tweets will be Retweeted
+SOURCE_USERNAMES = [
+    "user1",
+    "user2",
+    "user3",
+    ]

64e43908b1766c8db88309344434d14eb9aaf38b
Chris Mastris <cmastris@hotmail.com>
2021-07-18 13:48:48 +0100

    Add `api` attribute to Bot

--- tests/test_bot.py
+++ tests/test_bot.py
@@ -6,4 +6,4 @@ from top_tweets.bot import Bot
 class TestBot:
     def test_bot_init_assert(self):
         with pytest.raises(AssertionError):
-            Bot(usernames=["test"], user_ids=["test"])
+            Bot(None, usernames=["test"], user_ids=["test"])
--- top_tweets/bot.py
+++ top_tweets/bot.py
@@ -5,6 +5,7 @@ class Bot:
     """Quote Tweet or Retweet the top Tweets by other Twitter users.
 
     Attributes:
+        api (tweepy.API): Tweepy API object authenticated with the bot account credentials.
         usernames (list of str or None): a list of Twitter user screen names/handles (without "@")
             whose top Tweets will be shared. Either `usernames` or `user_ids` must be None.
         user_ids (list of str or None): a list of Twitter user unique identifiers whose top Tweets
@@ -16,9 +17,10 @@ class Bot:
             Uses `likes_retweets_combined` by default.
 
     """
-    def __init__(self, usernames=None, user_ids=None, metric="likes_retweets_combined"):
+    def __init__(self, api, usernames=None, user_ids=None, metric="likes_retweets_combined"):
         assert usernames is None or user_ids is None, "Either `usernames` or `user_ids` must be " \
                                                       "None."
+        self.api = api
         self.usernames = usernames
         self.user_ids = user_ids
         self.metric = metric

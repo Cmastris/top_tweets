52b7b944bcdc631415c34db6729355494eb22f9d
Chris Mastris <cmastris@hotmail.com>
2021-07-31 14:28:59 +0100

    Adjust API error retry attempts/delay

--- top_tweets/twitter_auth.py
+++ top_tweets/twitter_auth.py
@@ -7,8 +7,7 @@ def tweepy_auth():
     """Return an authenticated Tweepy API object."""
     auth = tweepy.OAuthHandler(config.CONSUMER_KEY, config.CONSUMER_SECRET)
     auth.set_access_token(config.ACCESS_TOKEN, config.ACCESS_TOKEN_SECRET)
-    # TODO: set retry values (10, 30) for production use
-    return tweepy.API(auth, retry_count=0, retry_delay=0)
+    return tweepy.API(auth, retry_count=10, retry_delay=30)
 
 
 API = tweepy_auth()
